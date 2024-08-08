8f4c121d453357d858b5befd507d4ef5af3f7f73
app-house-listing <catakeas@gmail.com>
2024-08-08 16:33:06 +0200

    Layout

--- src/SimpleAsync.tsx
+++ src/SimpleAsync.tsx
@@ -1,14 +1,21 @@
+// interface AnimeData {
+//   id: number;
+//   anime: string;
+//   character: string;
+//   quote: string;
+// }
+
 const SimpleAsync = () => {
   return (
     <div>
-      <div className="my-40 p-6 max-w-sm mx-auto bg-slate-800 rounded-xl shadow-lg flex flex-wrap md:flex-wrap-reverse items-center space-x-4">
+      <div className="my-60 p-6 max-w-sm mx-auto bg-slate-800 rounded-xl shadow-lg flex flex-wrap md:flex-wrap-reverse items-center space-x-4">
         <div className="font-mono flex-shrink space-y-5">
-          <h1 className=" text-3xl font-mono text-teal-400 md:text-teal-100 ">
-            “I'm selfish, impatient and a little insecure. I make mistakes, I am
-            out of control and at times hard to handle. But if you can't handle
-            me at my worst, then you sure as hell don't deserve me
+          <h1 className=" text-3xl font-mono text-teal-100 text-clip overflow-hidden">
+            “You've gotta dance like there's nobody watching, Love like you'll
+            never be hurt, Sing like there's nobody listening, And live like
+            it's heaven on earth.”
           </h1>
-          <h2 className="text-2xl text-teal-50">Character:</h2>
+          <h2 className="text-2xl text-lime-200	">Character:</h2>
           <h2 className="text-2xl text-teal-600">Anime:</h2>
         </div>
       </div>

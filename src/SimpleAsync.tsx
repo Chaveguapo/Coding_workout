8287f9ec69b71ea5e44c468f0ae08690453f1c22
app-house-listing <catakeas@gmail.com>
2024-08-08 14:15:37 +0200

    structure of app

--- src/SimpleAsync.tsx
+++ src/SimpleAsync.tsx
@@ -1,10 +1,16 @@
 const SimpleAsync = () => {
   return (
     <div>
-      <div>
-        <h2>Character:</h2>
-        <h3>Anime:</h3>
-        <h1 className="text-3xl f font-sans ">Quote</h1>
+      <div className="my-40 p-6 max-w-sm mx-auto bg-slate-800 rounded-xl shadow-lg flex flex-wrap md:flex-wrap-reverse items-center space-x-4">
+        <div className="font-mono flex-shrink space-y-5">
+          <h1 className=" text-3xl font-mono text-teal-400 md:text-teal-100 ">
+            “I'm selfish, impatient and a little insecure. I make mistakes, I am
+            out of control and at times hard to handle. But if you can't handle
+            me at my worst, then you sure as hell don't deserve me
+          </h1>
+          <h2 className="text-2xl text-teal-50">Character:</h2>
+          <h2 className="text-2xl text-teal-600">Anime:</h2>
+        </div>
       </div>
     </div>
   );

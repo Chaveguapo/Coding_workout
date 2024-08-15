12e1da57d3708c836b0d4cdc5abb0e063584916d
Chaveguapo <catakeas@gmail.com>
2024-08-15 14:26:58 +0200

    Update testing env

---
+++ src/Test/Test_QuotesBB.spec.js
@@ -0,0 +1,6 @@
+import { test, expect } from "vitest";
+import QuotesBreakingBad from "../assets/Components/QuotesBreakingBad";
+
+test("Fetch data from API and stored in a variable", () => {
+  expect(QuotesBreakingBad());
+});

0c5f567bf0d889d014fa3aecfe75f64b978ad715
Chaveguapo <catakeas@gmail.com>
2024-08-26 12:14:50 +0200

    changes on parallax

--- src/assets/Components/ParallaxContainer.tsx
+++ src/assets/Components/ParallaxContainer.tsx
@@ -1,15 +1,5 @@
-import QuotesBreakingBad from "./QuotesBreakingBad.tsx";
-import QuotesGot from "./QuotesGot.tsx";
-
 const ParallaxContainer = () => {
-  return (
-    <div className="parallax-container">
-      <div className="parallax-section">
-        <QuotesBreakingBad />
-      </div>
-      <QuotesGot />
-    </div>
-  );
+  return <div></div>;
 };
 
 export default ParallaxContainer;
